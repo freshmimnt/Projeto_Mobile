0d8f998c95261c65820a7351feeefeda60fdb087
Carlos <94853801+asantos142@users.noreply.github.com>
2023-11-15 18:25:24 +0000

    Mudança de id nos constraint layouts

--- Lazuli/app/src/main/java/pt/iade/projetomobile/lazuli/UcActivity.java
+++ Lazuli/app/src/main/java/pt/iade/projetomobile/lazuli/UcActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class DiscActivity extends AppCompatActivity {
+public class UcActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_disc);
+        setContentView(R.layout.activity_uc);
     }
 }

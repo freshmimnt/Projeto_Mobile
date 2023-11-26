b58cf80b5e94eb2edf2087a004b19db8c977ff1e
Carlos <94853801+asantos142@users.noreply.github.com>
2023-11-26 00:30:54 +0000

    Adição dos novos layouts

--- Lazuli/app/src/main/java/pt/iade/projetomobile/lazuli/LoginActivity.java
+++ Lazuli/app/src/main/java/pt/iade/projetomobile/lazuli/LoginActivity.java
@@ -6,21 +6,54 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button next;
+    private EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button next = (Button)findViewById(R.id.nextButton);
+        next = findViewById(R.id.nextButton);
+        email = findViewById(R.id.emailText);
+        password = findViewById(R.id.passText);
+
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, IntroActivity.class);
-                startActivity(intent);
+                if (isValidInput(email.getText().toString(), password.getText().toString())) {
+                    Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, IntroActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Por favor insira um email e password!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
+
+    private boolean isValidInput(String email, String password) {
+        return !email.isEmpty() && !password.isEmpty();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("username", email.getText().toString());
+        outState.putString("password", password.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        email.setText(savedInstanceState.getString("username", ""));
+        password.setText(savedInstanceState.getString("password", ""));
+    }
+
+
 }
